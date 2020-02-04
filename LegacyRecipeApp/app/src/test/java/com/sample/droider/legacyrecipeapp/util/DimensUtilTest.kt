52ee72ab6b7ba1bcd40e356ea31fea71b9fcf764
木本恭平 <gomigomigomim@gmail.com>
2020-02-04 21:30:32 +0900

    [add]テストクラスの追加

---
+++ LegacyRecipeApp/app/src/test/java/com/sample/droider/legacyrecipeapp/util/DimensUtilTest.kt
@@ -0,0 +1,43 @@
+package com.sample.droider.legacyrecipeapp.util
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class DimensUtilTest {
+    @Mock
+    private lateinit var mockContext: Context
+    @Mock
+    private lateinit var metrics: DisplayMetrics
+    @Mock
+    private lateinit var resources: Resources
+
+    @Test
+    fun dpToPxForPixel() {
+        metrics.xdpi = 420.0F
+
+        `when`(mockContext.resources).thenReturn(resources)
+        `when`(resources.displayMetrics).thenReturn(metrics)
+
+        // expected: 実際にEmulatorでビルドした時の値を適用
+        Assert.assertEquals(21, DimensUtil.dpToPx(8, mockContext))
+    }
+
+    @Test
+    fun dpToPx2ForNexus6() {
+        metrics.xdpi = 560.0F
+
+        `when`(mockContext.resources).thenReturn(resources)
+        `when`(resources.displayMetrics).thenReturn(metrics)
+
+        // expected: 実際にEmulatorでビルドした時の値を適用
+        Assert.assertEquals(28, DimensUtil.dpToPx(8, mockContext))
+    }
+}
